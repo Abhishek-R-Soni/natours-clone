45ec67df78677c8bcff8a7d5c79ab4065bc44e57
Abhishek Soni <abhishek.soni@shoptimize.in>
2021-12-03 09:56:04 +0530

    secure token via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,6 +8,28 @@ const signToken = (id) => {
     })
 };
 
+const createSendToken = (user, statusCode, res) => {
+    const token = signToken(user._id)
+
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE_IN * 24 * 60 * 60 * 1000 ), //  expires in 1d
+        httpOnly: true
+    }
+
+    res.cookie('jwt', token, cookieOptions)
+
+    // Remove password from output
+    user.password = undefined
+
+    res.status(statusCode).json({
+        status: 'success',
+        token,
+        data: {
+            user
+        }
+    });
+};
+
 exports.signup = async (req, res, next) => {
     try{
         const newUser = await User.create({
@@ -18,13 +40,7 @@ exports.signup = async (req, res, next) => {
             passwordChangeAt: req.body.passwordChangeAt
         })
         
-        const token = signToken(newUser._id);
-
-        res.status(201).json({
-            status: "success",
-            token: token,
-            data: newUser
-        })
+        createSendToken(newUser, 201, res);
     }
     catch(err){
         res.status(400).json({
@@ -48,13 +64,7 @@ exports.login = async (req, res, next) => {
         if(!user || !(await user.correctPassword(password, user.password))) return res.status(400).json({message: 'Incorrect email or password!'})
        
         // 3. if everything is ok. send token
-        const token = signToken(user._id)
-
-        res.status(200).json({
-            status: "success",
-            token,
-            data: user
-        })
+        createSendToken(user, 200, res);
     }
     catch(err){
         res.status(400).json({
